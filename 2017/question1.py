a907fc24f9a34aeb84ebb25832deb0d4f0ffacfc
sylviazhang <sylvia.zhang88@outlook.com>
2021-12-21 13:50:12 -0500

    :)

---
+++ 2017/question1.py
@@ -0,0 +1,16 @@
+x = int(input())
+y = int(input())
+
+if x > 0 and y > 0:
+    print("1")
+
+if x < 0 < y:
+    print("2")
+
+if x < 0 and y < 0:
+    print("3")
+
+if x > 0 > y:
+    print("4")
+
+# finished in 3 minutes 42 seconds
