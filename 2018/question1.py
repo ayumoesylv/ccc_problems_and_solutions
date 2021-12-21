e6d1dddc8a653ff1fa56532f1a4292942680ab3b
sylviazhang <sylvia.zhang88@outlook.com>
2021-12-21 17:40:03 -0500

    :)

---
+++ 2018/question1.py
@@ -0,0 +1,13 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+
+if (num1 == 8 or 9) and (num2 == num3) and (num4 == 8 or 9):
+    print("ignore")
+
+else:
+    print("answer")
+
+# finished in 10 minutes
+# Not sure how to complete this question
