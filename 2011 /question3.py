a7e6d1d7998e023c5a6f0a87dfaf94c96063453a
sylviazhang <sylvia.zhang88@outlook.com>
2021-12-27 20:52:48 -0500

    :)

--- 2011 /question3.py
+++ 2011 /question3.py
@@ -5,12 +5,24 @@ list = [t1, t2]
 i = 0
 idex = 0
 
-while i >= 0:
-    i = list[idex] - list[idex + 1]
-    term = i
-    list.append(term)
-    idex += 1
+# while i >= 0:
+# i = list[idex] - list[idex + 1]
+# term = i
+# list.append(term)
+# idex += 1
 
-print(len(list) - 1)
+while True:
+    i = list[idex] - list[idex + 1]
+    if i >= 0:
+        list.append(i)
+        idex +=1
+
+    else:
+        break
+
+print(len(list))
 
 # finished in 11 minutes 54 seconds
+
+# corrections
+# fixed bug in while loop
